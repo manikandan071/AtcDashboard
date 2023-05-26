e57c709fb8c823039abb1a8b71be10eec32370fb
Leowilson17 <leobca2018@gmail.com>
2023-05-26 13:13:58 +0530

    Test Push

--- src/webparts/fieldQualityDashboard/components/MainComponent.tsx
+++ src/webparts/fieldQualityDashboard/components/MainComponent.tsx
@@ -2,9 +2,5 @@ import * as React from "react";
 import Dashboard from "./Dashboard";
 
 export default function MainComponent(props: any): JSX.Element {
-  return (
-    <div>
-      <Dashboard spcontext={props.spcontext} />
-    </div>
-  );
+  return <div>{/* <Dashboard spcontext={props.spcontext} /> */}</div>;
 }
