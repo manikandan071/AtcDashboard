654d6c72561609a590d8fc5d7a9d59ad6ff97ada
Leowilson17 <leobca2018@gmail.com>
2023-05-26 13:15:30 +0530

    Test Done by Leo

--- src/webparts/fieldQualityDashboard/components/MainComponent.tsx
+++ src/webparts/fieldQualityDashboard/components/MainComponent.tsx
@@ -2,5 +2,9 @@ import * as React from "react";
 import Dashboard from "./Dashboard";
 
 export default function MainComponent(props: any): JSX.Element {
-  return <div>{/* <Dashboard spcontext={props.spcontext} /> */}</div>;
+  return (
+    <div>
+      <Dashboard spcontext={props.spcontext} />
+    </div>
+  );
 }
